bbbe7c0cf84cd2311c3cbf014406a43ecab39ec3
Amin Shah Gilani <amin@gilani.me>
2018-10-08 22:34:07 +0500

    Finish the config

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -8,15 +8,23 @@
 provider "google" {
   credentials = "${file("secret-account.json")}"
   project     = "worklark-218609"
-  zone      = "us-central1-a"
+  zone        = "us-central1-a"
 }
 
 # Node pool configuration
 resource "google_container_node_pool" "np" {
+  name       = "worklark-node-pool"
   cluster    = "${google_container_cluster.primary.name}"
   node_count = 3
+
+  node_config {
+    machine_type = "f1-micro"
+    disk_size_gb = 10         # Set the initial disk size
+    preemptible  = true
+  }
+
   management {
-    auto_repair = true
+    auto_repair  = true
     auto_upgrade = true
   }
 }
@@ -24,13 +32,8 @@ resource "google_container_node_pool" "np" {
 # configuration
 resource "google_container_cluster" "primary" {
   name               = "worklark-cluster"
-  initial_node_count = 3
-
-  node_config {
-    machine_type = "f1-micro"
-    disk_size_gb = 10 # Set the initial disk size
-    preemptible = true
-  }
+  logging_service    = "none"
+  monitoring_service = "none"
 
   addons_config {
     kubernetes_dashboard {
@@ -40,6 +43,26 @@ resource "google_container_cluster" "primary" {
     http_load_balancing {
       disabled = false # Configure the Kubernetes dashboard
     }
+  }
 
+  lifecycle {
+    ignore_changes = ["node_pool"]
+  }
+
+  node_pool {
+    name = "default-pool"
+  }
+}
+
+resource "google_compute_firewall" "default" {
+  name        = "http-https"
+  network     = "${google_container_cluster.primary.network}"
+  description = "Enable HTTP and HTTPS access"
+
+  direction = "INGRESS"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443"]
   }
 }
