37d5ec68e192b4e4292406db13e22deff264954a
Amin Shah Gilani <amin@gilani.me>
2018-10-12 05:55:47 +0500

    Allow the cluster service account to interact with databases

--- terraform/service-accounts.tf
+++ terraform/service-accounts.tf
@@ -1,6 +1,16 @@
+# based on the instructions under at
+# https://web.archive.org/web/20181012004032/https://cloud.google.com/sql/docs/postgres/connect-kubernetes-engine
+
 resource "google_service_account" "cluster_account" {
   account_id   = "cluster-account"
   display_name = "Cluster account"
+}
+
+resource "google_project_iam_binding" "admin-account-iam" {
+    role = "roles/cloudsql.client"
+    members = [
+      "serviceAccount:${google_service_account.cluster_account.email}",
+    ]
 }
 
 resource "google_service_account_key" "cluster_key" {
